567984058874824bfd7773d4a1116ca8c5750299
shilpishah <shilpishah2003@gmail.com>
2025-04-12 17:44:18 -0400

    Create functions.py
    masterlist of helper functions

---
+++ functions.py
@@ -0,0 +1,17 @@
+# read given input .pickle file 
+def read_pickle(file_path):
+    try:
+        with open(file_path, 'rb') as file:
+            data = pickle.load(file)
+            return data
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+        return None
+
+# add gaussian noise
+def add_gaussian_noise(Z, sigma=0.1):
+    noise = np.random.normal(0, sigma, Z.shape)
+    wigner_noisy = Z + noise
+    # print(wigner_noisy) - used for testing
+    return wigner_noisy
+
